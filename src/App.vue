2d824253a73c59e2fc15bf01c63cf4fbc3c4bf65
yeqiu2010 <820771543@qq.com>
2019-04-24 22:33:37 +0800

    vue2 cesium结合模板

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import cesiumViewer from "./components/cesiumViewer";
+
+export default {
+  name: "App",
+  components: {
+    cesiumViewer: cesiumViewer
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+  width: 100%;
+}
+
+html， body， #cesiumContainer {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+</style>
---
+++ src/components/cesiumViewer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<script>
+import Viewer from "cesium/Source/Widgets/Viewer/Viewer";
+import buildModuleUrl from "cesium/Source/Core/buildModuleUrl";
+import "cesium/Source/Widgets/widgets.css";
+
+export default {
+  name: "cesiumContainer",
+  mounted: function() {
+    buildModuleUrl.setBaseUrl("../../static/Cesium/");
+    this.viewer = new Viewer("cesiumContainer");
+  },
+  data() {
+    return {
+      viewer: {}
+    };
+  }
+};
+</script>
+
+<style>
+</style>
